25dadad668d71f6a3308cb39a5bbf766ac071b46
napitts <pitts.nate@gmail.com>
2023-05-21 23:35:14 -0700

    Add initial data model

--- server/opm_backend/backend_mvp/models.py
+++ server/opm_backend/backend_mvp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+
+class Package(models.Model):
+    """ Represents an Odin Package """
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    maintainers = models.CharField(max_length=200)
+    created = models.DateTimeField("date created")
+    # todo change versions so it points to version object(s) below
+    versions = models.CharField(max_length=200)
+    # Type could be Lib, Demo etc.
+    type = models.CharField(max_length=200)
+    license = models.CharField(max_length=200)
+
+
+class Version(models.Model):
+    """ Represents a single Odin Package Version """
+    id = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    created = models.DateTimeField("date created")
+    # State could be alpha, beta, pre-published, final etc.
+    state = models.CharField(max_length=200)
+
+
+class Org(models.Model):
+    """ Represents an Organization (ex: Core Dev Team, Microsoft, Google etc.) """
+    name: CharField = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    created = models.DateTimeField("date created")
--- server/opm_backend/backend_mvp/views.py
+++ server/opm_backend/backend_mvp/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, odin. You're at the backend_mvp index.")
 
+
 def api_list(request):
     return HttpResponse("Hello, odin. You're at the backend_mvp api list")

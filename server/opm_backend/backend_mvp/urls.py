f981ac551d91d52e5b46d782bd13cdb430abc898
napitts <pitts.nate@gmail.com>
2023-05-24 07:44:31 -0700

    Separate interactive and api views, clean up

--- server/opm_backend/backend_mvp/urls.py
+++ server/opm_backend/backend_mvp/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 app_name = "backend_mvp"
 urlpatterns = [
+    # Interactive
     path("", views.index, name="index"),
-    path("api/v0/view-api-routes", views.view_api_routes),
-    path("api/v0/view-packages", views.view_packages),
-    path("api/v0/package/<int:package_id>/", views.view_pkg_detail),
-    path("api/v0/package/<int:version_id>/", views.view_ver_detail),
+    path("view-api-routes", views.view_api_routes, name="view_api_routes"),
+    path("view-packages", views.view_packages, name="view_packages"),
+    path("package/<int:package_id>/", views.view_pkg_detail, name="view_pkg_detail"),
+    path("package/<int:package_id>/version/<int:version_id>", views.view_ver_detail, name="view_ver_detail"),
+    # API
     path("api/v0/packages", views.packages),
 ]
--- server/opm_backend/backend_mvp/views.py
+++ server/opm_backend/backend_mvp/views.py
@@ -21,9 +21,10 @@ def view_pkg_detail(request, package_id):
     return render(request, "backend_mvp/view_pkg_detail.html", {"pkg": pkg})
 
 
-def view_ver_detail(request, version_id):
+def view_ver_detail(request, package_id, version_id):
+    pkg = get_object_or_404(Package, pk=package_id)
     ver = get_object_or_404(Version, pk=version_id)
-    return render(request, "backend_mvp/view_ver_detail.html", {"ver": ver})
+    return render(request, "backend_mvp/view_ver_detail.html", {"pkg": pkg, "ver": ver})
 
 
 def packages(request):
@@ -60,6 +61,11 @@ def del_package(request):
 
 
 def create_version(request):
+    ver = get_object_or_404(Version, pk=version_id)
+    return HttpResponse("todo")
+
+
+def organizations(request):
     ver = get_object_or_404(Version, pk=version_id)
     return HttpResponse("todo")
 

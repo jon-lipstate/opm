af50da234b42be5c3642338e47ce959b518094de
Jon Lipstate <Jon@Lipstate.com>
2023-05-30 22:38:14 -0700

    prod diagnistics

--- src/routes/api/readme/+server.ts
+++ src/routes/api/readme/+server.ts
@@ -24,19 +24,22 @@ export async function POST(event) {
 			throw error(res.status, res.statusText);
 		}
 		data = '```odin\n' + res.data + '\n```';
-		console.warn('TODO: REMOVE ODIN TAG WRAPPER');
+		console.info('TODO: REMOVE ODIN TAG WRAPPER');
 	} else {
 		throw error(400, "readme requires 'data' or 'url' to process.");
 	}
+	console.info('RENDER');
 	const rendered = md.render(data);
 	const dom = new JSDOM(rendered);
+	console.info('HLJS');
 	dom.window.document.querySelectorAll('pre').forEach((x) => {
-		hljs.highlightBlock(x);
+		hljs.highlightElement(x);
 		// hljs.highlightAuto(x, { language: 'odin' });
 	});
 	let html = dom.window.document.documentElement.outerHTML;
+	console.info('PURIFY');
 	const DOMPurify = createDOMPurify(dom.window);
 	html = DOMPurify.sanitize(html);
-
+	console.info('API COMPLETE');
 	return json({ html });
 }
